e2144040b4932ad547c175251eee55be86c30102
dj <727656871@qq.com>
2022-05-08 15:36:26 +0800

    更新

---
+++ gmall-realtime/src/main/java/com/djin/gmallrealtime/common/NewUserCheckFunction.java
@@ -0,0 +1,45 @@
+package com.djin.gmallrealtime.common;
+
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 该类使用状态做新老用户校验
+ */
+public class NewUserCheckFunction extends RichMapFunction<JSONObject, JSONObject> {
+    //                    声明状态用于表示当前Mid是否已经访问过
+    private ValueState<String> firstVisitDateState;
+    private SimpleDateFormat simpleDateFormat;
+    @Override
+    public void open(Configuration parameters) throws Exception{
+        firstVisitDateState = getRuntimeContext().getState(
+                new ValueStateDescriptor<String>("new_mid", String.class));
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    }
+    @Override
+    public JSONObject map(JSONObject value) throws Exception {
+        //                        取出新用户标记
+        String isNew = value.getJSONObject("commen").getString("is_new");
+//                        如果当前前端传输数据表示为新用户，则进行校验
+        if ("1".equals(isNew)){
+//                            取出状态数据并取出当前访问时间
+            String firstDate = firstVisitDateState.value();
+            Long ts = value.getLong("ts");
+//                            判断状态数据是否为Null
+            if (firstDate != null) {
+//                                修复
+                value.getJSONObject("common").put("is_new", "0");
+            } else {
+//                                更新状态
+                firstVisitDateState.update(simpleDateFormat.format(ts));
+            }
+        }
+        return value;
+    }
+}
---
+++ gmall-realtime/src/main/java/com/djin/gmallrealtime/common/SideOutputFunction.java
@@ -0,0 +1,44 @@
+package com.djin.gmallrealtime.common;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+/**
+ * 分流，使用ProcessFunction将ODS数据拆分成启动、曝光以及页面数据
+ */
+public class SideOutputFunction extends ProcessFunction<JSONObject,String> {
+    @Override
+    public void processElement(JSONObject value, ProcessFunction<JSONObject, String>.Context ctx, Collector<String> out) throws Exception {
+        //                        提取”start“字段
+        String start = value.getString("start");
+//                        判断是否为启动数据
+        if (start != null && start.length() > 0) {
+//                            将启动日志输出到侧输出流
+            ctx.output(new OutputTag<String>("start") {
+                       },
+                    value.toString());
+        } else {
+//                            为页面数据，将数据输出到主流
+            out.collect(value.toString());
+//                            不是启动数据，继续判断是否是曝光数据
+            JSONArray displays = value.getJSONArray("displays");
+            if (displays != null && displays.size() > 0) {
+//                                为曝光数据，遍历写入侧输出流
+                for (int i = 0; i < displays.size(); i++) {
+//                                    取出单条曝光数据
+                    JSONObject displayJson = displays.getJSONObject(i);
+//                                    添加页面ID
+                    displayJson.put("page_id",
+                            value.getJSONObject("page").getString("page_id"));
+//                                    输出到侧输出流
+                    ctx.output(new OutputTag<String>("display") {
+                               },
+                            displayJson.toString());
+                }
+            }
+        }
+    }
+}
